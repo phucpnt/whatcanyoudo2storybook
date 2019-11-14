9b0ead62727ca17348aff92ae6f961c280d439cd
Phuc PNT <phuc.phan@sentifi.com>
2019-11-14 14:49:47 +0700

    feat: jest integration for snapshot testing

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,8 @@
 import { configure, addDecorator } from '@storybook/react';
 import { withR2Request } from "storybook-addon-r2request/lib/index";
 
+import 'isomorphic-fetch';
+
 addDecorator(withR2Request);
 
 // automatically import all files ending in *.stories.js
